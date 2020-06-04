de9d33712c5b0718213a677f7e4d71e643b43ec5
eallion@eallion.com <eallion@eallion.com>
2020-06-04 10:55:35 +0800

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# About
+
+这是一个基于 [Cup](https://github.com/CoolQ/Cup) 的 VPS status 状态监控页。托管在 [Git Pages](https://eallion.github.io).
+
+## 源码
+
+Cup: [https://github.com/CoolQ/Cup](https://github.com/CoolQ/Cup)  
+
+## 演示
+
+- [酷Q状态](https://status.cqp.cc)
+- [**VPS status**](https://status.eallion.com) 
---
+++ js/config.js
@@ -0,0 +1,19 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm780109022-4ac13fd210d048a7d96b8979',		// Openwrt Router
+	'm783953686-5912db7169eea9ce488b60c1',		// Blog Home Page
+	'm784979395-421a3e5f4d5c472e17cbd746',		// CDN Origin
+	'm780108929-9178e0b3e091daa86ec2a7f3',		// Aliyun HK
+	'm781833645-500dd4676d2f8f1d57199e6c',		// Aliyun HZ
+	'm780624347-31661dfd1ec086b180801040',		// Tencent GZ
+	'm783531673-15c8868ee165e3c80807b7c0',		// Google HK
+	'm783953743-37e6e7168359948ba0151260',		// AWS HK
+	'm783953751-fec9f51f660997d5efe77857',		// Oracle Cloud
+	'm781450763-856d189f18a8095b76b8c9ae',		// BWGhost DC6
+	'm783953731-f12e6f295ce9a2720a14d8e1',		// Sy Umbrella Home Page
+	'm784005570-6e1ea056361f4533dc4f8474',		// Git Pages
+	'm784005562-c9e0731436f32bc4c2e819ab',		// Coding Pages
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
