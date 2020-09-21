954c0755eea5def13f341ced0fd0fff5f4d9a18a
Charles Chin <eallions@gmail.com>
2020-09-21 18:34:59 +0800

    update

--- js/config.js
+++ js/config.js
@@ -6,13 +6,16 @@ var __apiKeys = [
 	'm780108929-9178e0b3e091daa86ec2a7f3',		// Aliyun HK
 	'm781833645-500dd4676d2f8f1d57199e6c',		// Aliyun HZ
 	'm780624347-31661dfd1ec086b180801040',		// Tencent GZ
+	'm786001804-54b06025361b876683101a6b',		// Tencent HK
 	'm783531673-15c8868ee165e3c80807b7c0',		// Google HK
 	'm783953743-37e6e7168359948ba0151260',		// AWS HK
 	'm783953751-fec9f51f660997d5efe77857',		// Oracle Cloud
+	'm786001814-07f298862bb7a36f0a376d31',		// IBM Cloud
 	'm781450763-856d189f18a8095b76b8c9ae',		// BWGhost DC6
 	'm783953731-f12e6f295ce9a2720a14d8e1',		// Sy Umbrella Home Page
 	'm784005570-6e1ea056361f4533dc4f8474',		// Git Pages
 	'm784005562-c9e0731436f32bc4c2e819ab',		// Coding Pages
+	'm786001812-b7751d2877906a1be0480bc6',		// Gitee Pages
 ];
 
 // refresh interval (in seconds)
