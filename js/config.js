4929eb08f9941a16d7f9205640021af0f54ec343
Charles Chin <eallions@gmail.com>
2020-09-21 18:38:42 +0800

    update

--- js/config.js
+++ js/config.js
@@ -2,6 +2,8 @@
 var __apiKeys = [
 	'm780109022-4ac13fd210d048a7d96b8979',		// Openwrt Router
 	'm783953686-5912db7169eea9ce488b60c1',		// Blog Home Page
+	'm786001835-9304248beafa5670e3ee9ec1',		// dl.eallion.com
+	'm786001832-268c89617f727ef0ce3eec2d',		// api.eallion.com
 	'm784979395-421a3e5f4d5c472e17cbd746',		// CDN Origin
 	'm780108929-9178e0b3e091daa86ec2a7f3',		// Aliyun HK
 	'm781833645-500dd4676d2f8f1d57199e6c',		// Aliyun HZ
@@ -12,7 +14,7 @@ var __apiKeys = [
 	'm783953751-fec9f51f660997d5efe77857',		// Oracle Cloud
 	'm786001814-07f298862bb7a36f0a376d31',		// IBM Cloud
 	'm781450763-856d189f18a8095b76b8c9ae',		// BWGhost DC6
-	'm783953731-f12e6f295ce9a2720a14d8e1',		// Sy Umbrella Home Page
+	'm783953731-f12e6f295ce9a2720a14d8e1',		// Umbrella Home Page
 	'm784005570-6e1ea056361f4533dc4f8474',		// Git Pages
 	'm784005562-c9e0731436f32bc4c2e819ab',		// Coding Pages
 	'm786001812-b7751d2877906a1be0480bc6',		// Gitee Pages
